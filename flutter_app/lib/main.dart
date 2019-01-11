e1b7711ba5edf8e28dc42a6fab8c483de019ec11
we0z <weoz@ukr.net>
2019-01-11 11:39:25 +0200

    add flutter module

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Hello my world",
+    home: new Scaffold(
+      body: Text("Yeah! Look, I'm here!!!"),
+    ),
+  ));
+}
